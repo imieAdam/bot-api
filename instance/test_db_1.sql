62a04db4c4fa5b7001163463e4ed84820d0fec36
imieAdam <50846353+imieAdam@users.noreply.github.com>
2022-10-23 22:33:36 +0200

    fixed db instance

--- instance/test_db_1.sql
+++ instance/test_db_1.sql
@@ -1,15 +1,5 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "locations" (
-	"location_id"	INTEGER,
-	"address"	TEXT NOT NULL,
-	PRIMARY KEY("location_id")
-);
-INSERT INTO locations VALUES(1,'ul. Opolska 110');
-INSERT INTO locations VALUES(2,'ul. Jasnogórska 11');
-INSERT INTO locations VALUES(3,'ul. Lublańska 38');
-INSERT INTO locations VALUES(4,'al. Pokoju 5');
-INSERT INTO locations VALUES(5,'ul. Wadowicka 7');
 CREATE TABLE IF NOT EXISTS "visit_to_location" (
 	"visit_to_location_id"	INTEGER,
 	"visit_id"	INTEGER NOT NULL,
@@ -22,6 +12,12 @@ INSERT INTO visit_to_location VALUES(3,1,2);
 INSERT INTO visit_to_location VALUES(4,2,4);
 INSERT INTO visit_to_location VALUES(5,3,1);
 INSERT INTO visit_to_location VALUES(6,3,2);
+INSERT INTO visit_to_location VALUES(7,4,5);
+INSERT INTO visit_to_location VALUES(8,4,1);
+INSERT INTO visit_to_location VALUES(9,4,2);
+INSERT INTO visit_to_location VALUES(10,5,5);
+INSERT INTO visit_to_location VALUES(11,5,3);
+INSERT INTO visit_to_location VALUES(12,5,2);
 CREATE TABLE IF NOT EXISTS "specializations" (
 	"specialization_id"	INTEGER,
 	"specialization"	TEXT NOT NULL,
@@ -44,6 +40,8 @@ INSERT INTO time_frames VALUES(2,1,'15:30','22:00');
 INSERT INTO time_frames VALUES(3,2,'07:00','10:00');
 INSERT INTO time_frames VALUES(4,2,'15:00','22:00');
 INSERT INTO time_frames VALUES(5,3,'09:00','11:00');
+INSERT INTO time_frames VALUES(6,4,'08:11','09:31');
+INSERT INTO time_frames VALUES(7,5,'08:11','09:01');
 CREATE TABLE IF NOT EXISTS "users" (
 	"user_id"	INTEGER,
 	"user_name"	TEXT NOT NULL UNIQUE,
@@ -51,6 +49,7 @@ CREATE TABLE IF NOT EXISTS "users" (
 	PRIMARY KEY("user_id")
 );
 INSERT INTO users VALUES(1,'admin','admin');
+INSERT INTO users VALUES(2,'admin2','admin2');
 CREATE TABLE IF NOT EXISTS "user_portal_credentials" (
 	"user_portal_credentials_id"	INTEGER,
 	"login_email"	TEXT NOT NULL,
@@ -69,7 +68,29 @@ CREATE TABLE IF NOT EXISTS "visits" (
 	"user_id"	INTEGER NOT NULL,
 	PRIMARY KEY("visit_id")
 );
-INSERT INTO visits VALUES(1,1,'2020-10-01','2020-09-25T19:00:00.000',NULL,NULL,1);
-INSERT INTO visits VALUES(2,5,'2020-10-01','2020-11-25T19:00:00.000',NULL,NULL,1);
+INSERT INTO visits VALUES(1,2,'2020-10-01','2020-11-25T21:37:00.000',1,'Joe Doe',1);
+INSERT INTO visits VALUES(2,5,'2020-10-01','2020-11-25T19:00:00.000',1,'Joe Doe',1);
 INSERT INTO visits VALUES(3,4,'2020-10-10',NULL,NULL,NULL,1);
+INSERT INTO visits VALUES(4,3,'2020-11-01',NULL,NULL,NULL,2);
+INSERT INTO visits VALUES(5,1,'2020-11-01',NULL,NULL,NULL,2);
+CREATE TABLE IF NOT EXISTS "locations" (
+	"location_id"	INTEGER,
+	"address"	TEXT NOT NULL,
+	"city_id"	INTEGER NOT NULL DEFAULT 0,
+	PRIMARY KEY("location_id")
+);
+INSERT INTO locations VALUES(1,'ul. Opolska 110',1);
+INSERT INTO locations VALUES(2,'ul. Jasnogórska 11',1);
+INSERT INTO locations VALUES(3,'ul. Lublańska 38',1);
+INSERT INTO locations VALUES(4,'al. Pokoju 5',1);
+INSERT INTO locations VALUES(5,'ul. Wadowicka 7',1);
+INSERT INTO locations VALUES(6,'ul. Starowiejska 189',2);
+INSERT INTO locations VALUES(7,'ul. Krakowska 10',2);
+CREATE TABLE IF NOT EXISTS "cities" (
+	"city_id"	INTEGER,
+	"name"	TEXT,
+	PRIMARY KEY("city_id")
+);
+INSERT INTO cities VALUES(1,'Kraków');
+INSERT INTO cities VALUES(2,'Radomsko');
 COMMIT;
